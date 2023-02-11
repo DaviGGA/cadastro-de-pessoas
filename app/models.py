a6e52e80051bc8041cea679fa1802c70fdabfa70
DaviGGA <davigregorio.a@hotmail.com>
2023-02-11 19:35:04 -0300

    CRUD completed

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    cep = models.CharField(max_length=9)
+    house_number = models.PositiveIntegerField()
+    street = models.CharField(max_length=30)
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    address = models.ForeignKey(Address, on_delete= models.CASCADE)
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from . models import *
+from django.shortcuts import redirect
 
-# Create your views here.
+def index(request):
+
+    persons = Person.objects.all()
+   
+    if request.method == 'POST':
+        name = request.POST['name']
+        age = request.POST['age']
+        street = request.POST['street']
+        number = request.POST['number']
+        cep = request.POST['cep']
+
+        address = Address.objects.create(street = street, house_number = number, cep = cep)
+        person = Person.objects.create(name = name, age = age, address = address)
+
+        address.save()
+        person.save()
+
+        return redirect('/')
+
+          
+    return render(request, 'index.html', {'persons' : persons})
+
+
+def delete(request,pk):
+    if request.method == 'POST':
+        person = Person.objects.filter(pk = pk).first()
+        person.delete()
+
+        return redirect ('/')
+
+def edit (request,pk):
+    person = Person.objects.filter(pk = pk).first()
+
+    if request.method == 'POST':
+        name = request.POST['e-name']
+        age = request.POST['e-age']
+        street = request.POST['e-street']
+        number = request.POST['e-number']
+        cep = request.POST['e-cep']
+
+        person.name = name
+        person.age = age
+        person.address.street = street
+        person.address.house_number = number
+        person.address.cep = cep
+        
+        person.save()
+
+        return redirect ('/')
+
+    return render(request, 'edit.html', {'person' : person})
